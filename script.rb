8d018e5d613752d01c8a8ec4c87c9e40d3fe9da3
Mon Ouïe <mon.ouie@gmail.com>
2010-08-22 10:50:52 +0200

    Removed loggin message from script.rb

--- script.rb
+++ script.rb
@@ -4,14 +4,7 @@ def path_of(res)
   File.expand_path(File.join(File.dirname(__FILE__), "spec", "res", res))
 end
 
-puts "requiring ray..."
 require 'ray'
-
-puts "requiring rspec..."
 require 'spec'
-
-puts "loading specs..."
 Dir["spec/**/*_spec.rb"].each { |file| load file }
-
-puts "running spec"
 Spec::Runner.run
