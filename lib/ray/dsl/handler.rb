7a922de1256d89859f884d574553fc224b8b6645
Mon ouïe <mon.ouie@gmail.com>
2010-08-23 22:40:16 +0200

    Aded special behavior for matchers and regexps in handlers

--- lib/ray/dsl/handler.rb
+++ lib/ray/dsl/handler.rb
@@ -7,7 +7,7 @@ module Ray
 
       def match?(event)
         return false unless event.type == @type
-        return @args == event.args unless @args.empty?
+        return match_args?(event.args) unless @args.empty?
         true
       end
 
@@ -21,6 +21,32 @@ module Ray
 
       attr_reader :type
       attr_reader :args
+
+      private
+      def match_args?(args)
+        return false if @args.size != args.size
+
+        @args.each_with_index do |elem, i|
+          other = args[i]
+
+          case elem
+          when Ray::DSL::Matcher
+            return false unless elem.match?(other)
+          when Regexp
+            if other.is_a? Regexp
+              return false unless elem == other
+            elsif other.is_a? String
+              return false unless elem =~ other
+            else
+              return false
+            end
+          else
+            return false unless elem == other
+          end
+        end
+
+        return true
+      end
     end
   end
 end
--- spec/ray/event_spec.rb
+++ spec/ray/event_spec.rb
@@ -1,11 +1,18 @@
 describe "the event DSL" do
   context "when someone listens to an event" do
+    before :all do
+      Ray.describe_matcher(:more_than, :integer) do |x|
+        lambda { |i| i > x }
+      end
+    end
+
     before :each do
       @runner = Ray::DSL::EventRunner.new
 
       @obj = Object.new
       @obj.extend Ray::DSL::Listener
       @obj.extend Ray::DSL::EventRaiser
+      @obj.extend Ray::Matchers
 
       @obj.listener_runner = @runner
       @obj.raiser_runner = @runner
@@ -42,6 +49,66 @@ describe "the event DSL" do
 
         @runner.run
         var.should == 1
+      end
+    end
+
+    context "if a matcher is one of them" do
+      it "should send the events if it matches the argument" do
+        count = 0
+
+        @obj.instance_eval do
+          on :foo, more_than(10) do |x|
+            x.should > 10
+            count += 1
+          end
+
+          raise_event(:foo, 5)
+          raise_event(:foo, "a")
+          raise_event(:foo, 15)
+        end
+
+        @runner.run
+        count.should == 1
+      end
+    end
+
+    context "if a regex is one of them" do
+      it "should send the event if a string match it" do
+        count = 0
+
+        @obj.instance_eval do
+          on :foo, /hello/ do |str|
+            str.should =~ /hello/
+            count += 1
+          end
+
+          raise_event(:foo, 0)
+          raise_event(:foo, "wat?")
+          raise_event(:foo, "Hey, hello you!")
+          raise_event(:foo, /hey hello you/)
+        end
+
+        @runner.run
+        count.should == 1
+      end
+
+      it "should send then event if the argument is the same regex" do
+        count = 0
+
+        @obj.instance_eval do
+          on :foo, /hello/ do |reg|
+            reg.should == /hello/
+            count += 1
+          end
+
+          raise_event(:foo, 0)
+          raise_event(:foo, 5.5)
+          raise_event(:foo, "Goodbye!")
+          raise_event(:foo, /hello/)
+        end
+
+        @runner.run
+        count.should == 1
       end
     end
 
