0885e3c8acaee987ee8f3eb1d4acda572ce792ef
Mon ouïe <mon.ouie@gmail.com>
2010-08-23 18:33:18 +0200

    Added spec for type

---
+++ spec/ray/type_spec.rb
@@ -0,0 +1,17 @@
+describe "Ray.resolve_type" do
+  context "when given a module" do
+    it "should return it" do
+      Ray.resolve_type(Class).should == Class
+    end
+  end
+
+  context "when given something else" do
+    it "should return nil if it's not registred" do
+      Ray.resolve_type(3).should == nil
+    end
+
+    it "should return a module if it is registred" do
+      Ray.resolve_type(:string).should == String
+    end
+  end
+end
