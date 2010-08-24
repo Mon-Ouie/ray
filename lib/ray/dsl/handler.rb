37cfc9e4116f5bf8cdc52ec67c2211fcb3305dcf
Mon ouïe <mon.ouie@gmail.com>
2010-08-24 14:28:37 +0200

    Handlers should not try to convert matchers

--- lib/ray/dsl/handler.rb
+++ lib/ray/dsl/handler.rb
@@ -10,6 +10,9 @@ module Ray
 
         if desc = Ray.description_for_event(@type)
           desc.each_with_index do |type, i|
+            next if @args[i].is_a? Matcher
+            next if @args[i].is_a? Regexp
+
             begin
               @args[i] = Ray.convert(@args[i], type)
             rescue TypeError
