c2ac474e2fdf97aa36f61102c0f68944bdf72578
Mon ouïe <mon.ouie@gmail.com>
2010-08-26 15:29:35 +0200

    Catching mouse_press events

--- lib/ray/dsl/event_translator.rb
+++ lib/ray/dsl/event_translator.rb
@@ -9,6 +9,14 @@ module Ray
         Ray::Event::APPACTIVE     => :app_activity
       }
 
+      MOUSE_BUTTON = {
+        Ray::Event::BUTTON_LEFT      => :left,
+        Ray::Event::BUTTON_MIDDLE    => :middle,
+        Ray::Event::BUTTON_RIGHT     => :right,
+        Ray::Event::BUTTON_WHEELUP   => :wheel_up,
+        Ray::Event::BUTTON_WHEELDOWN => :wheel_down
+      }
+
       class << self
         # @return [Array, nil] The arguments you should pass to raise_event.
         def translate_event(ev)
@@ -17,6 +25,7 @@ module Ray
           when Ray::Event::TYPE_QUIT then quit(ev)
           when Ray::Event::TYPE_ACTIVEEVENT then active_event(ev)
           when Ray::Event::TYPE_MOUSEMOTION then mouse_motion(ev)
+          when Ray::Event::TYPE_MOUSEBUTTONDOWN then mouse_press(ev)
           else []
           end
         end
@@ -31,6 +40,12 @@ module Ray
 
         def mouse_motion(ev)
           [[:mouse_motion, Ray::Rect.new(ev.mouse_x, ev.mouse_y)]]
+        end
+
+        def mouse_press(ev)
+          [[:mouse_press,
+            MOUSE_BUTTON[ev.mouse_button],
+            Ray::Rect.new(ev.mouse_x, ev.mouse_y)]]
         end
       end
     end
