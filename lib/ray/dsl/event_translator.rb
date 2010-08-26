802efb0a595fa82551872225203b16d4910e22f9
Mon ouïe <mon.ouie@gmail.com>
2010-08-26 16:46:34 +0200

    Handling more events

--- lib/ray/dsl/event_translator.rb
+++ lib/ray/dsl/event_translator.rb
@@ -3,13 +3,13 @@ module Ray
     # Module charged to translate an instance of Ray::Event into the arguments
     # you pass to raise_event.
     module EventTranslator
-      FOCUS_TYPE = {
+      FOCUS_TYPES = {
         Ray::Event::APPMOUSEFOCUS => :mouse_focus,
         Ray::Event::APPINPUTFOCUS => :input_focus,
         Ray::Event::APPACTIVE     => :app_activity
       }
 
-      MOUSE_BUTTON = {
+      MOUSE_BUTTONS = {
         Ray::Event::BUTTON_LEFT      => :left,
         Ray::Event::BUTTON_MIDDLE    => :middle,
         Ray::Event::BUTTON_RIGHT     => :right,
@@ -26,6 +26,9 @@ module Ray
           when Ray::Event::TYPE_ACTIVEEVENT then active_event(ev)
           when Ray::Event::TYPE_MOUSEMOTION then mouse_motion(ev)
           when Ray::Event::TYPE_MOUSEBUTTONDOWN then mouse_press(ev)
+          when Ray::Event::TYPE_MOUSEBUTTONUP then mouse_release(ev)
+          when Ray::Event::TYPE_KEYDOWN then key_press(ev)
+          when Ray::Event::TYPE_KEYUP then key_release(ev)
           else []
           end
         end
@@ -35,7 +38,8 @@ module Ray
         end
 
         def active_event(ev)
-          [[ev.is_gain? ? :focus_gain : :focus_loss, FOCUS_TYPE[ev.focus_state]]]
+          [[ev.is_gain? ? :focus_gain : :focus_loss,
+            FOCUS_TYPES[ev.focus_state]]]
         end
 
         def mouse_motion(ev)
@@ -44,8 +48,22 @@ module Ray
 
         def mouse_press(ev)
           [[:mouse_press,
-            MOUSE_BUTTON[ev.mouse_button],
+            MOUSE_BUTTONS[ev.mouse_button],
             Ray::Rect.new(ev.mouse_x, ev.mouse_y)]]
+        end
+
+        def mouse_release(ev)
+          [[:mouse_release,
+            MOUSE_BUTTONS[ev.mouse_button],
+            Ray::Rect.new(ev.mouse_x, ev.mouse_y)]]
+        end
+
+        def key_press(ev)
+          [[:key_press, ev.key, ev.mod_keys]]
+        end
+
+        def key_release(ev)
+          [[:key_release, ev.key, ev.mod_keys]]
         end
       end
     end
