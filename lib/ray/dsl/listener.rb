fdc73ba25186c9473f55c11be9990ab9c277543f
Mon ouïe <mon.ouie@gmail.com>
2010-08-27 14:40:00 +0200

    Updated documentatin about arguments for handlers

--- lib/ray/dsl/listener.rb
+++ lib/ray/dsl/listener.rb
@@ -4,10 +4,13 @@ module Ray
     module Listener
       # Registers a block to listen to an event.
       #
+      # Arguments are compared with the ones of the regex using === and,
+      # if it failed, ==.
+      #
       # @param [Symbol] event Name of the event to listen to
-      # @param [DSL::Matcher, Regexp, Object>] args List of arguments that should
-      #                                             match the event's (if empty,
-      #                                             the block will be called anyway)
+      # @param [DSL::Matcher, Regexp, Object] args List of arguments that should
+      #                                            match the event's (if empty,
+      #                                            the block will be called anyway)
       def on(event, *args, &block)
         return unless listener_runner
         listener_runner.add_handler(event, args, block)
