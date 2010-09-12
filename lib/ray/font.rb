ad2ce8c8ae943004a34385439c1b01695a57df54
Mon ouïe <mon.ouie@gmail.com>
2010-09-12 13:47:27 +0200

    Added String#draw

--- lib/ray/font.rb
+++ lib/ray/font.rb
@@ -21,3 +21,80 @@ module Ray
     end
   end
 end
+
+class String
+  # Draws the receiver.
+  #
+  # @option opts [Ray::Font] :font The font used to render the string.
+  # @option opts [Ray::Image] :on The image to draw on.
+  # @option opts [Integer] :w (:width) and :h (:height) Size of the image
+  #                        if :on isn't specified. :w is required in that case.
+  # @option opts [Symbol] The encoding. Can be guessed in Ruby 1.9.
+  # @option opts [Ray::Color] :color The color to draw the text in.
+  # @option opts [Symbol] :mode The drawing mode.
+  # @option opts [Array<Symbol>] :style The different styles to apply.
+  #                                     :italic, :bold, and :underlined.
+  #
+  # @option opts [Array<Integer>] :at Where the image should be drawn.
+  #                                   Defaults to (0, 0)
+  # @see Ray::Font#draw
+  def draw(opts = {})
+    font = opts[:font]
+
+    lines = split(/\r\n|\n|\r/)
+    line_skip = font.line_skip
+
+    target = opts[:on]
+
+    target ||= Ray::Image.new(:height => opts[:height] || opts[:h] ||
+                              line_skip * lines.size,
+                              :width  => opts[:width] || opts[:w])
+
+
+    string_encoding = opts[:encoding]
+    string_encoding ||= if respond_to? :encoding # Ruby 1.9
+                          case encoding.to_s
+                          when /^utf-?8$/i
+                            :utf8
+                          when /^iso-8859-/i
+                            :latin1
+                          else
+                            nil
+                          end
+                        else
+                          nil
+                        end
+
+    color      = opts[:color]
+    background = opts[:background]
+
+    mode = opts[:mode]
+
+    if styles = opts[:style]
+      font.style = styles.inject(0) do |flags, style|
+        flags |= case style
+                 when :italic
+                   Ray::Font::STYLE_ITALIC
+                 when :bold
+                   Ray::Font::STYLE_BOLD
+                 when :underlined
+                   Ray::Font::STYLE_UNDERLINE
+                 else
+                   raise "Unknown flag #{style}"
+                 end
+      end
+    end
+
+    x, y = opts[:at]
+    x ||= 0
+    y ||= 0
+
+    lines.each do |line|
+      font.draw(line, :on => target, :at => [x, y], :encoding => string_encoding,
+                :color => color, :background => background, :mode => mode)
+      y += line_skip
+    end
+
+    target
+  end
+end
--- spec/ray/font_spec.rb
+++ spec/ray/font_spec.rb
@@ -49,4 +49,51 @@ if Ray.has_font_support?
       Ray.stop
     end
   end
+
+  describe "String#draw" do
+    before :all do
+      Ray.init
+
+      @win  = Ray.create_window(:w => 100, :h => 100)
+      @font = Ray::Font.new(path_of("VeraMono.ttf"), 12)
+    end
+
+    it "should ask for the width unless :on is specified" do
+      lambda {
+        "Hello world!".draw(:font => @font)
+      }.should raise_exception
+    end
+
+    it "should not ask for the width if :on is specified" do
+      lambda {
+        "Hello world!".draw(:font => @font, :on => @win)
+      }.should_not raise_exception
+    end
+
+    it "should use the specified width unless :on is specified" do
+      img = "Hello world!".draw(:font => @font, :w => 40)
+      img.w.should == 40
+    end
+
+    it "should require the font to be specified" do
+      lambda {
+        "Hello world".draw(:on => @win)
+      }.should raise_exception
+    end
+
+    it "should draw on multiple lines" do
+      img = "Hello\nworld".draw(:font => @font, :w => 100)
+      img.height.should == @font.line_skip * 2
+    end
+
+    it "should allow to specify an explicit size" do
+      img = "Hello\nworld".draw(:font => @font, :h => 100, :w => 50)
+      img.width.should == 50
+      img.height.should == 100
+    end
+
+    after :all do
+      Ray.stop
+    end
+  end
 end
