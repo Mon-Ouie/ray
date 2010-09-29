f14cb48fca60808109357ed266230900298becb0
Mon ouïe <mon.ouie@gmail.com>
2010-09-30 00:20:57 +0200

    Allowed the C parser to parse constants

--- yard_ext.rb
+++ yard_ext.rb
@@ -15,3 +15,39 @@ class MatcherHandler < YARD::Handlers::Ruby::Base
     end
   end
 end
+
+class CustomCParser < YARD::Parser::CParser
+  include YARD
+
+  def parse
+    super
+    parse_constants
+  end
+
+  private
+
+  def handle_constants(type, var_name, const_name, definition)
+    namespace = @namespaces[var_name]
+    obj = CodeObjects::ConstantObject.new(namespace, const_name)
+    obj.value = definition
+  end
+
+  def parse_constants
+    @content.scan(%r{\Wrb_define_
+                   ( variable          |
+                     readonly_variable |
+                     const             |
+                     global_const      | )
+               \s*\(
+                 (?:\s*(\w+),)?
+                 \s*"(\w+)",
+                 \s*(.*?)\s*\)\s*;
+                 }xm) do |type, var_name, const_name, definition|
+      var_name = "rb_cObject" if !var_name or var_name == "rb_mKernel"
+      handle_constants type, var_name, const_name, definition
+    end
+  end
+end
+
+YARD::Parser::SourceParser.register_parser_type(:c, CustomCParser,
+                                                ['c', 'cc', 'cxx', 'cpp'])
