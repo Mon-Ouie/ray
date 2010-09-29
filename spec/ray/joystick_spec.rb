eca18f2d7a5baf9b37d3b4d349fdebf4e1c0b212
Mon ouïe <mon.ouie@gmail.com>
2010-09-29 20:31:09 +0200

    Changed whitespaces in joystick_spec.rb

--- spec/ray/joystick_spec.rb
+++ spec/ray/joystick_spec.rb
@@ -2,20 +2,20 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Ray::Joystick do
   describe ".handle_event" do
-      before :all do
-        Ray.init
-      end
+    before :all do
+      Ray.init
+    end
 
-      it "can be changed" do
-        Ray::Joystick.handle_event = true
-        Ray::Joystick.handle_event.should be_true
+    it "can be changed" do
+      Ray::Joystick.handle_event = true
+      Ray::Joystick.handle_event.should be_true
 
-        Ray::Joystick.handle_event = false
-        Ray::Joystick.handle_event.should be_false
-      end
+      Ray::Joystick.handle_event = false
+      Ray::Joystick.handle_event.should be_false
+    end
 
-       after :all do
-        Ray.init
+    after :all do
+      Ray.init
     end
   end
 end
