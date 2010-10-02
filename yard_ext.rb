2d0f14ecf9aa02ca2d2a10e3d7820dd327dc4bf5
Mon ouïe <mon.ouie@gmail.com>
2010-10-02 09:39:30 +0200

    Made the C parser find method body

--- yard_ext.rb
+++ yard_ext.rb
@@ -26,6 +26,45 @@ class CustomCParser < YARD::Parser::CParser
 
   private
 
+  def find_method_body(object, func_name, content = @content)
+    case content
+    when %r"((?>/\*.*?\*/\s*))(?:(?:static|SWIGINTERN)\s+)?(?:intern\s+)?VALUE\s+#{func_name}
+                \s*(\([^)]*\))([^;]|$)"xm
+      comment, params = $1, $2
+      body_text = $&
+
+      remove_private_comments(comment) if comment
+
+      # see if we can find the whole body
+
+      re = Regexp.escape(body_text.strip) + '[^(]*\{.*?^\}'
+      puts re
+      if /#{re}/m =~ content
+        body_text = $&
+      end
+
+      # The comment block may have been overridden with a 'Document-method'
+      # block. This happens in the interpreter when multiple methods are
+      # vectored through to the same C method but those methods are logically
+      # distinct (for example Kernel.hash and Kernel.object_id share the same
+      # implementation
+
+      # override_comment = find_override_comment(object)
+      # comment = override_comment if override_comment
+
+      object.docstring = parse_comments(object, comment) if comment
+      object.source = body_text
+    when %r{((?>/\*.*?\*/\s*))^\s*\#\s*define\s+#{func_name}\s+(\w+)}m
+      comment = $1
+      find_method_body(object, $2, content)
+    else
+      # No body, but might still have an override comment
+      # comment = find_override_comment(object)
+      comment = nil
+      object.docstring = parse_comments(object, comment) if comment
+    end
+  end
+
   def handle_constants(type, var_name, const_name, definition)
     namespace = @namespaces[var_name]
     obj = CodeObjects::ConstantObject.new(namespace, const_name)
