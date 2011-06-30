e6b077a917df3e024bdf4d8521efd69454053f48
Mon ouïe <mon.ouie@gmail.com>
2011-06-30 13:57:25 +0200

    Made Window#initialize open the window if an argument is pased to it

--- lib/ray/window.rb
+++ lib/ray/window.rb
@@ -1,5 +1,12 @@
 module Ray
   class Window
+    # @param title (see #open)
+    # @param size  (see #open)
+    # @param opts  (see #open)
+    def initialize(title = nil, size = [640, 480], opts = {})
+      open(title, size, opts) if title
+    end
+
     # @yieldparam [Ray::Event] event Each event to be processed
     def each_event
       return Enumerator.new(self, :each_event) unless block_given?
