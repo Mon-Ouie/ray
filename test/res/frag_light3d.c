a18804ab4dc468c760f8ecdb807294ebec1336e1
Mon ouïe <mon.ouie@gmail.com>
2011-07-12 20:58:24 +0200

    Changed shader a bit

--- test/res/frag_light3d.c
+++ test/res/frag_light3d.c
@@ -11,11 +11,9 @@ varying vec3 var_Normal;
 void main() {
   float intensity = dot(in_Light, normalize(var_Normal));
 
-  float factor = 0.6;
-  if      (intensity > 1.0)  factor = 1.0;
-  else if (intensity > 0.7)  factor = 0.8;
-  else if (intensity > 0.25) factor = 0.4;
-  else if (intensity < 0.0)  factor = 0.1;
+  float factor = 0.5;
+  if (intensity > 0.1) factor = 1.0;
+  else if (intensity < 0.0)  factor = 0.2;
 
   vec4 light = vec4(factor, factor, factor, 1.0);
   gl_FragColor = var_Color * light;
