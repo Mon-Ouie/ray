056d1675ce3acf25dac59ea0a07620963c3e00b8
Mon ouïe <mon.ouie@gmail.com>
2011-07-13 22:08:38 +0200

    Made starfighter work on 1.8

--- samples/starfighter/starfighter.rb
+++ samples/starfighter/starfighter.rb
@@ -12,61 +12,61 @@ end
 
 class Player
   include Ray::Helper
-  
+
   extend Forwardable
   def_delegators :@sprite, :x, :y, :x=, :y=, :pos, :pos=
-  
+
   attr_reader :score, :window, :sprite
 
-  def initialize(stars) 
-    @stars = stars  
+  def initialize(stars)
+    @stars = stars
     @sprite = Ray::Sprite.new path_of("_media/Starfighter.png")
     @sprite.origin = @sprite.image.size / 2
-    
+
     @beep = sound path_of("_media/Beep.wav")
-    
+
     @vel_x = @vel_y = 0.0
-    
-    @score = 0    
+
+    @score = 0
   end
-  
+
   def register(scene)
     self.event_runner = scene.event_runner
     @window = scene.window
   end
-  
+
   def update
     if holding? key(:up)
       @vel_x += Math::sin(@sprite.angle / (180 / Math::PI)) * 0.5
       @vel_y -= Math::cos(@sprite.angle / (180 / Math::PI)) * 0.5
     end
-    
+
     if holding? key(:left)
       @sprite.angle -= 4.5
     end
-    
+
     if holding? key(:right)
       @sprite.angle += 4.5
     end
-    
+
     move
-    
+
     collect_stars
   end
 
   def warp(position)
     @sprite.pos = position
   end
-  
+
   def move
     self.pos += [@vel_x, @vel_y]
     self.x %= window.size.width
     self.y %= window.size.height
-    
+
     @vel_x *= 0.95
     @vel_y *= 0.95
   end
-  
+
   def collect_stars
     @stars.reject! do |star|
       if pos.dist(star.pos) < 35 then
@@ -82,36 +82,37 @@ end
 
 class Star
   include Ray::Helper
-  
+
   extend Forwardable
   def_delegators :@sprite, :x, :y, :x=, :y=, :pos
 
-  attr_reader :sprite  
-  
-  def initialize(position)  
+  attr_reader :sprite
+
+  def initialize(position)
     @sprite = Ray::Sprite.new path_of("_media/Star.png"), :at => position
     @sprite.sheet_size = [10, 1]
     @sprite.origin = (@sprite.image.size / @sprite.sheet_size) / 2
-    @animation = sprite_animation(:from => [0, 0], :to => [9.5, 0], :duration => 2).start(@sprite)
-    
+    @animation = sprite_animation(:from => [0, 0], :to => [9.5, 0],
+                                  :duration => 2).start(@sprite)
+
     color = Ray::Color.white
-    color.red = rand(255 - 40) + 40
+    color.red   = rand(255 - 40) + 40
     color.green = rand(255 - 40) + 40
-    color.blue = rand(255 - 40) + 40
+    color.blue  = rand(255 - 40) + 40
     @sprite.color = color
   end
-  
+
   def register(scene)
     self.event_runner = scene.event_runner
     @animation.event_runner = event_runner
-    
+
     on :animation_end, @animation do
       @animation.start @sprite
     end
-  end    
-  
+  end
+
   def update
-    @animation.update   
+    @animation.update
   end
 end
 
@@ -120,34 +121,35 @@ Ray::Game.new("Starfighter --- [LEFT/RIGHT/UP to move; collect the stars]") do
     add_hook :quit, method(:exit!)
   end
 
-  scene :game do   
+  scene :game do
     @background_image = sprite path_of("_media/Space.png")
-    @score_text = text "", at: [10, 10], size: 20, color: Ray::Color.yellow
-    
+    @score_text = text("", :at => [10, 10], :size => 20,
+                       :color => Ray::Color.yellow)
+
     @stars = Array.new
-    
+
     @player = Player.new @stars
     @player.register self
     @player.warp(window.size / 2)
-    
+
     add_hook :key_press, key(:escape), method(:exit!)
-    
-    always do     
+
+    always do
       if rand(100) < 4 and @stars.size < 25 then
         star = Star.new [window.size.width * rand, window.size.height * rand]
         star.register self
         @stars << star
       end
-      
-      @stars.each(&:update)
-      @player.update 
 
-      @score_text.string = "Score: #{@player.score}"      
+      @stars.each(&:update)
+      @player.update
+
+      @score_text.string = "Score: #{@player.score}"
     end
-    
-    render do |win|    
+
+    render do |win|
       win.draw @background_image
-      @stars.each {|star| win.draw star.sprite }   
+      @stars.each {|star| win.draw star.sprite }
       win.draw @player.sprite
       win.draw @score_text
     end
