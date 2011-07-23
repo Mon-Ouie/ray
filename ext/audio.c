c128e5f5ebcb58cd24ad04019b202477bd0c65d0
Mon ouïe <mon.ouie@gmail.com>
2011-07-23 21:51:14 +0200

    Made documentation in audio.c more consistent

--- ext/audio.c
+++ ext/audio.c
@@ -3,18 +3,18 @@
 VALUE ray_mAudio = Qnil;
 
 /*
-  @return [Float] The global volume. 0 is the minimum volume, whereas 100
-  is the maximum.
-*/
+ * @see volume=
+ */
 VALUE ray_audio_volume(VALUE self) {
   return rb_float_new(say_audio_get_volume());
 }
 
 /*
-  @overload volume=(val)
-  Sets the global volume.
-  @param [Float] val The new value for the volume.
-*/
+ * @overload volume=(val)
+ *   Sets the global volume
+ *   @param [Float] val The new value for the volume, 0 being the minimum and
+ *     100 the maximum as well as the default.
+ */
 VALUE ray_audio_set_volume(VALUE self, VALUE value) {
   float volume = (float)NUM2DBL(value);
   say_audio_set_volume(volume);
@@ -23,17 +23,17 @@ VALUE ray_audio_set_volume(VALUE self, VALUE value) {
 }
 
 /*
-  @return [Ray::Vector3] The position of the listener.
-*/
+ * @see pos=
+ */
 VALUE ray_audio_pos(VALUE self) {
   return ray_vector3_to_rb(say_audio_get_pos());
 }
 
 /*
-  @overload pos=(pos)
-  Sets the position of the listener.
-  @param [Ray::Vector3, #to_vector3] pos The new position of the
-  listener.
+ * @overload pos=(pos)
+ *   Sets the position of the listener
+ *   @param [Ray::Vector3, #to_vector3] pos The new position of the
+ *    listener.
 */
 VALUE ray_audio_set_pos(VALUE self, VALUE pos) {
   say_audio_set_pos(ray_convert_to_vector3(pos));
@@ -41,22 +41,28 @@ VALUE ray_audio_set_pos(VALUE self, VALUE pos) {
 }
 
 /*
-  @return [Ray::Vector3] The orientation of the listener.
-*/
+ * @see direction=
+ */
 VALUE ray_audio_direction(VALUE self) {
   return ray_vector3_to_rb(say_audio_get_direction());
 }
 
 /*
-  @overload direction=(dir)
-  Sets the orientation of the listener.
-  @param [Ray::Vector3, #to_vector3] dir The new orientation of the listener.
-*/
+ * @overload direction=(dir)
+ *   Sets the orientation of the listener
+ *   @param [Ray::Vector3, #to_vector3] dir The new orientation of the listener.
+ */
 VALUE ray_audio_set_direction(VALUE self, VALUE dir) {
   say_audio_set_direction(ray_convert_to_vector3(dir));
   return dir;
 }
 
+/*
+ * Document-class: Ray::Audio
+ *
+ * Ray.:Audio is used to manipulate the state of the listener, through global
+ * parameters such as volume or position in the 3D world.
+ */
 void Init_ray_audio() {
   ray_mAudio = rb_define_module_under(ray_mRay, "Audio");
   rb_define_module_function(ray_mAudio, "volume", ray_audio_volume, 0);
