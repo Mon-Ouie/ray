9e6a36a7bed0b66841351babf627d6147e0a55b1
Mon ouïe <mon.ouie@gmail.com>
2011-08-04 12:12:43 +0200

    Added new line at end of file

--- ext/say_thread.c
+++ ext/say_thread.c
@@ -7,18 +7,18 @@ static unsigned int say_thread_entry_point(void *data) {
   say_thread *th = (say_thread*)data;
   th->func(th->data);
   _endthreadex(0);
-  
+
   return 0;
 }
 
 say_thread *say_thread_create(void *data, say_thread_func func) {
   say_thread *th = malloc(sizeof(say_thread));
-  
+
   th->func = func;
   th->data = data;
-  
+
   th->th = _beginthreadex(NULL, 0, say_thread_entry_point, th, 0, NULL);
-  
+
   return th;
 }
 
