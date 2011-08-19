8532e682c77ca6f60dc2a0178dded925d19c69e2
Mon ouïe <mon.ouie@gmail.com>
2011-08-19 21:55:50 +0200

    Allowed to have multiple effects in invert.rb

--- samples/effects/invert.rb
+++ samples/effects/invert.rb
@@ -14,8 +14,26 @@ Ray.game "grayscale" do
     @obj = sprite path_of("sprite.png")
 
     effect_generator do |gen|
-      gen << color_inversion
+      gen << color_inversion << grayscale
       gen.build window.shader
+    end
+
+    @states = Hash.new(true)
+
+    {
+      :color_inversion => :i,
+      :grayscale       => :g
+    }.each do |state, key_name|
+      on :key_press, key(key_name) do
+        @states[state] = !@states[state]
+        window.shader["#{state}.enabled"] = @states[state]
+      end
+    end
+
+    @inverted = true
+
+    on :key_press, key(:i) do
+
     end
 
     render do |win|
